d1be72f17889d43d134209c16241caec325cfcff
naseeba-salah <naseebasalah44@gmail.com>
2024-12-12 15:03:57 +0530

    Add files via upload

---
+++ week7/package/graphics/cuboid.py
@@ -0,0 +1,5 @@
+import math
+def cuboid_volume(l,b,h):
+    return l*b*h;
+def cuboid_tsa(l,b,h):
+    return 2*(l*b+b*h+h*l);
---
+++ week7/package/graphics/sphere.py
@@ -0,0 +1,7 @@
+import math
+def sphere_volume(r):
+    pi=3.14
+    return (4/3)*pi*r*r;
+def sphere_tsa(r):
+    pi=3.14
+    return 4*pi*r;
