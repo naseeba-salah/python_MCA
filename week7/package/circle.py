40c506e9bff7b62faa40145039d6cec44a10e87b
naseeba-salah <naseebasalah44@gmail.com>
2024-12-12 15:02:42 +0530

    Add files via upload

---
+++ week7/package/circle.py
@@ -0,0 +1,9 @@
+import math
+def circle_area(r):
+    pi=3.14
+    return pi*(r*r);
+
+def circle_perimeter(r):
+    pi=3.14
+    return 2*pi*r;
+    
---
+++ week7/package/rectangle.py
@@ -0,0 +1,5 @@
+import math
+def rectangle_area(l,b):
+    return l*b;
+def rectangle_perimeter(l,b):
+    return 2*(l+b);
