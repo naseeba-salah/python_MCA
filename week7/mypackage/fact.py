621d003515dcc9acc6216081889fbcf07dc1e1e3
naseeba-salah <naseebasalah44@gmail.com>
2024-12-12 15:05:48 +0530

    Add files via upload

---
+++ week7/mypackage/fact.py
@@ -0,0 +1,5 @@
+def factorial():
+    fact=1
+    for i in range(1,num+1):
+        fact=fact*i
+    print(fact)    
---
+++ week7/mypackage/fibo.py
@@ -0,0 +1,11 @@
+def fibonacci():
+    n1=0
+    n2=1
+    print(f"{a}{b}",end=" ")
+    for i in range(3,num+1):
+        c=a+b
+        print(f"{c}",end=" ")
+        a=b
+        b=c
+
+        
